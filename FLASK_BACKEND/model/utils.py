6580235c9d761ebbf5f2891cf4a2ef8a65a16520
Shayantan <shayantanbiswas137@gmail.com>
2025-06-13 12:39:55 +0530

    Changes

--- FLASK_BACKEND/model/utils.py
+++ FLASK_BACKEND/model/utils.py
@@ -1,7 +1,12 @@
-import cv2
 import os
+import torch
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+from PIL import Image
+import torch
+
 
 def predict_output():
+    
     folder = "Uploads"  # Placeholder for the actual image path
     
     list_dir = os.listdir(folder)
@@ -11,15 +16,28 @@ def predict_output():
     img_path = os.path.join(folder, list_dir[0])  # Get the first image in the directory
     
     
-    # ////////////////////
-    img = cv2.imread(img_path)
-    cv2.imshow("Show Image:",img)
-    cv2.waitKey(5000)
-    cv2.destroyAllWindows()
-    # ////////////////////
+    # Load processor and model
+    processor = AutoImageProcessor.from_pretrained("rajistics/finetuned-indian-food", use_fast=True)
+    model = AutoModelForImageClassification.from_pretrained("rajistics/finetuned-indian-food")
+
+    image = Image.open(img_path).convert("RGB")  # Replace with the actual image path
     
+        
+    # Preprocess
+    inputs = processor(images=image, return_tensors="pt")
+
+    # Forward pass
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    # Get prediction
+    logits = outputs.logits
+    predicted_class = logits.argmax(-1).item()
+    label = model.config.id2label[predicted_class]
+
     os.remove(img_path)  # Remove the image file (if needed, adjust this logic)
 
 
-    return "Image processed and removed successfully."
+    return {"Predicted class": label ,
+            "confidence": round(logits.max().item() , 2)}  # Format confidence to 2 decimal places
     
